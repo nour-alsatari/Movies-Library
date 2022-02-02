7c9e79102e4971f43941c8fcaafe1b890bf4f292
nour-alsatari <nouress98@gmail.com>
2022-02-02 21:02:39 +0200

    completed

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ node server.js to start the server
 ## Project Features
 
 my app is a movies library to check the latest movies based on categories
+
+## What observations or questions do you have about what you’ve learned so far?
+it needs a lot of practice
+
+## How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+it took me around 4 hours of reading and solving
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { default: axios } = require("axios");
 const express = require("express");
 
 const app = express();
-// app.use(express.json()); // it's an express thing i have to write so i can see the body of post request otherwise i will get undefined
+app.use(express.json()); // it's an express thing i have to write so i can see the body of post request otherwise i will get undefined
 
 const data = require("./Movie Data/data.json")
 
@@ -13,8 +13,8 @@ const pg = require('pg');
 
 dotenv.config(); // if i want to read anything inside .end has to be after the config
 const APIKEY = process.env.APIKEY;
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+// const bodyParser = require('body-parser');
+// const jsonParser = bodyParser.json();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 console.log(DATABASE_URL);
@@ -88,7 +88,8 @@ app.get('/searchMovie', (req, res) => {
 
 // end point to insert data to database, i need to use post because im sending data
 
-app.post('/addMovie', jsonParser, (req, res) => {
+app.post('/addMovie', (req, res) => {
+
     let movie = req.body // req.body to get the data
     // write query to insert this in my db
     const sql = `INSERT INTO moviesAdded (Adult ,overview ,title, release_date, poster_path, comment) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`
@@ -103,16 +104,61 @@ app.post('/addMovie', jsonParser, (req, res) => {
 // end point to get data from database so the user see the data they intered
 // i wanna get all the movies the user added 
 
-app.get('/getMovies' , (req, res) => {
+app.get('/getMovies', (req, res) => {
     const sql = `SELECT * FROM moviesAdded ;`
-    client.query(sql).then( data => {
+    client.query(sql).then(data => {
         return res.status(200).json(data.rows);
     })
-    
+
 })
+
+
+app.put('/UPDATE/:id', (req, res) => {
+
+    const id = req.params.id;
+    const movie = req.body // get the new data from the body
+    console.log(movie);
+    console.log(id);
+    const sql = `UPDATE moviesAdded SET Adult=$1, overview=$2, title=$3, release_date=$4, poster_path=$5, comment=$6 WHERE id=${id} RETURNING *;`;// i need to update all data
+
+    let values = [movie.Adult, movie.overview, movie.title, movie.release_date, movie.poster_path, movie.comment];
+
+    client.query(sql, values).then(data => {
+        return res.status(200).json(data.rows);
+
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+})
+
+// get addedmovie with a speecific endpoint 
+app.get('/getMovie/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const sql = `SELECT * FROM moviesAdded WHERE id=${id};`;
+
+    client.query(sql).then(data => {
+        res.status(200).json(data.rows);
+
+    })
+})
+
+app.delete('/DELETE/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = `DELETE FROM moviesAdded WHERE id=${id};`;
+    client.query(sql).then(() => {
+        return res.status(204).json([]);
+
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+})
+
+
 
 // client.connect() returns a promise but i dont want to connect to the server without my database first so i use.then
 client.connect().then(() => {
 
-    app.listen(3100, () => console.log("server started on 3000"));
+    app.listen(3200, () => console.log("server started on 3200"));
 })
